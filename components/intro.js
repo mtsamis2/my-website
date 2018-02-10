702ade5c5c2ef579f4eb1cc6c7ee37c014227b4b
mtsamis2 <mtsamis@gmail.com>
2018-02-10 16:07:05 -0500

    Separate intro section

---
+++ components/intro.js
@@ -0,0 +1,11 @@
+const Intro = () => (
+    <section id="intro">
+        <div className="logo"><img src="/static/images/logo.png" alt="" /></div>
+            <header>
+                <h2>Mike<br/>Tsamis</h2>
+                <p>IDEATE. CREATE. ITERATE. REPEAT.</p>
+            </header>
+    </section>
+)
+
+export default Intro
